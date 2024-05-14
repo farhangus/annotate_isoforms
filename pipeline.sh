2da6ca1a39b9f9466239767b71befe258f3d54c0
Farhang <fjaryanius@gmail.com>
2024-05-14 10:45:03 -0500

    updtae the output_path and prefix

--- pipeline.sh
+++ pipeline.sh
@@ -3,21 +3,21 @@
 # Function to display usage information
 display_aanotate_pipeline_usage() {
     echo "Options:"
-    echo "  -h,--help                          Display this help message"
-    echo "  -i,--input-file      <isoforms_list>     Input isoforms_list file"
-    echo "  -b,--bed       <provided_bedfile>  provided_bedfile "
-    echo "  -f,--fraction       Similarity fraction  default=0.9"
-    echo "  -r,--reference <REF>               reference fasta file"
-    # echo "  -m,--margin    base_pairs_margin   default=10"
-    echo "  -o,--output    output_fle_path   output file path"
+    echo "  -h,--help             Display this help message"
+    echo "  -o,--output           output_file_path          default=current_directory"
+    echo "  -f,--fraction         Similarity fraction       default=0.9"
+    echo "  -p,--ptrefix          prefix                    default="annotated_""
+    echo "  -i,--input-file       <isoforms_list>           Input isoforms_list file"
+    echo "  -b,--bed              <provided_bedfile>        provided_bedfile "
+    echo "  -r,--reference        <REF>                     reference fasta file"
 }
 
 # Initialize variables
 ISOFORMS_LIST=""
 PROVIDED_BEDFILE=""
 REFERENCE=""
-OUTPUT_FILE="annotated_isoforms.bed"
-MARGIN=0
+OUTPUT_FILE="./"
+PREFIX="annotated_"
 TOTAL_NUMBER_OF_ISOFORMS=0
 FOUND_ISOFORMS=0
 FRACTION=.9
@@ -42,10 +42,11 @@ while [[ $# -gt 0 ]]; do
             ;;
         -o|--output)
             OUTPUT_FILE=$2
+            mkdir -p ${OUTPUT_FILE}
             shift 2
             ;;
-        -m|--margin)
-            MARGIN=$2
+        -p|--prefix)
+            PREFIX=$2
             shift 2
             ;;
         -f|--fraction)
@@ -71,9 +72,9 @@ then
     display_aanotate_pipeline_usage
     exit 1
 fi
-isoforms_locations=$(mktemp)
-isoforms_locations='isoforms_locations.txt'
-grep -v '^\s*$' "$ISOFORMS_LIST" >tmp_${isoforms_locations}
+tmp_isoforms_locations=$(mktemp)
+isoforms_locations="${OUTPUT_FILE}/${PREFIX}isoforms_locations.txt"
+grep -v '^\s*$' "$ISOFORMS_LIST" >${tmp_isoforms_locations}
 rm -f $isoforms_locations
 # Iterate through each line in the ISOFORMS_LIST file
 while IFS= read -r isoform; do
@@ -84,96 +85,31 @@ while IFS= read -r isoform; do
     else 
         echo -e "_\t _\t _\t ${isoform}" >> $isoforms_locations
     fi
-done < tmp_${isoforms_locations}
-
+done < ${tmp_isoforms_locations}
+grep '^_*_*_' $isoforms_locations > "${OUTPUT_FILE}/${PREFIX}Not_found_isoforms.txt"
 ISOFORMS_NOT_FOUND=$(grep -c '^_*_*_' $isoforms_locations)
  
-grep -v '^_*_*_' ${isoforms_locations} > tmp_${isoforms_locations}
-cat tmp_${isoforms_locations} > ${isoforms_locations}
+grep -v '^_*_*_' ${isoforms_locations} > ${tmp_isoforms_locations}
+cat ${tmp_isoforms_locations} > ${isoforms_locations}
 
-echo -e "\e[31mTrinity Isoforms found  but not found in RefSeq \e[34m Fraction: ${FRACTION}\e[0m" > 'log.txt'
-bedtools intersect -a "$isoforms_locations" -b mm39_genes.bed -f $FRACTION  -wa -wb  -v >> 'log.txt'
-echo -e "\e[31mIsoforms intersection between Trinity and RefSeq \e[34m Fraction: ${FRACTION}\e[0m" >> 'log.txt'
-bedtools intersect -a "$isoforms_locations" -b mm39_genes.bed -f $FRACTION  -wa -wb  >> 'log.txt'
-awk '!seen[$1,$2,$3]++' log.txt  > "$OUTPUT_FILE"
-echo -e "\e[31m***************************************************************************************\e[0m" > STATS.log
+echo -e "\e[31mTrinity Isoforms found  but not found in RefSeq \e[34m Fraction: ${FRACTION}\e[0m" > "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
+bedtools intersect -a "$isoforms_locations" -b mm39_genes.bed -f $FRACTION  -wa -wb  -v >> "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
+echo -e "\e[31mIsoforms intersection between Trinity and RefSeq \e[34m Fraction: ${FRACTION}\e[0m" >> "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
+bedtools intersect -a "$isoforms_locations" -b mm39_genes.bed -f $FRACTION  -wa -wb  >> "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
+awk '!seen[$1,$2,$3]++' "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"  > "${OUTPUT_FILE}/${PREFIX}isoform_result.txt"
+echo -e "\e[31m***************************************************************************************\e[0m" > "${OUTPUT_FILE}/${PREFIX}STATS.txt"
 NUMBER_OF_ISOFORMS="$(grep -v '^\s*$' ${ISOFORMS_LIST} | wc -l  | grep -o '[0-9]*' )"
-awk '!seen[$4]++' "${OUTPUT_FILE}" > "uniq_${OUTPUT_FILE}"
-echo "Total Number of Isoforms: ${NUMBER_OF_ISOFORMS}" >> STATS.log
-echo "Isoforms Not Found: ${ISOFORMS_NOT_FOUND}" >> STATS.log
+awk '!seen[$4]++' "${OUTPUT_FILE}/${PREFIX}isoform_result.txt" > "${OUTPUT_FILE}/${PREFIX}uniq_isoform_result.txt"
+echo "Total Number of Isoforms: ${NUMBER_OF_ISOFORMS}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
+echo "Isoforms Not Found: ${ISOFORMS_NOT_FOUND}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
 
 
-ISOFORMS_TRINITY=$(awk '/Trinity Isoforms found /{flag=1; next} /Isoforms intersection between Trinity and RefSeq/{ exit} {if(flag) {count++}} END {print count}' "${OUTPUT_FILE}")
-ISOFORMS_TRINITY_UNIQ=$(awk '/Trinity Isoforms found /{flag=1; next} /Isoforms intersection between Trinity and RefSeq/{ exit} {if(flag) {count++}} END {print count}' "uniq_${OUTPUT_FILE}")
-echo "Number of Isoforms Exclusively Reported in Trinity: ${ISOFORMS_TRINITY}" >> STATS.log
-total_isoform_found=$(wc -l "${OUTPUT_FILE}"| grep -o '[0-9]*')
-total_isoform_found_uniq=$(wc -l "uniq_${OUTPUT_FILE}"| grep -o '[0-9]*')
-echo "Number of Shared Isoforms: $((total_isoform_found -ISOFORMS_TRINITY -2 ))" >> STATS.log
-echo "Number of Unique Isoforms Exclusively Reported in Trinity: ${ISOFORMS_TRINITY_UNIQ}" >> STATS.log
-echo "Number of Shared Unique Isoforms: $((total_isoform_found_uniq - ISOFORMS_TRINITY_UNIQ -2 ))" >> STATS.log
+ISOFORMS_TRINITY=$(awk '/Trinity Isoforms found /{flag=1; next} /Isoforms intersection between Trinity and RefSeq/{ exit} {if(flag) {count++}} END {print count}' "${OUTPUT_FILE}/${PREFIX}isoform_result.txt")
+ISOFORMS_TRINITY_UNIQ=$(awk '/Trinity Isoforms found /{flag=1; next} /Isoforms intersection between Trinity and RefSeq/{ exit} {if(flag) {count++}} END {print count}' "${OUTPUT_FILE}/${PREFIX}uniq_isoform_result.txt")
+echo "Number of Isoforms Exclusively Reported in Trinity: ${ISOFORMS_TRINITY}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
+total_isoform_found=$(wc -l "${OUTPUT_FILE}/${PREFIX}isoform_result.txt"| grep -o '[0-9]*')
+total_isoform_found_uniq=$(wc -l "${OUTPUT_FILE}/${PREFIX}uniq_isoform_result.txt"| grep -o '[0-9]*')
+echo "Number of Shared Isoforms: $((total_isoform_found -ISOFORMS_TRINITY -2 ))" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
+echo "Number of Unique Isoforms Exclusively Reported in Trinity: ${ISOFORMS_TRINITY_UNIQ}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
+echo "Number of Shared Unique Isoforms: $((total_isoform_found_uniq - ISOFORMS_TRINITY_UNIQ -2 ))" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
 
-# echo "Tr_chr,Tr_start_,Tr_end,Tr_isoform,Tr_gene,Ref_chrom,Ref_chromStart,Ref_chromEnd,Ref_name,Ref_geneName,Ref_geneName2,Ref_geneType"
-# while IFS= read -r line; do
-
-#     bedfile_chromosome=$(awk '{print $1}' <<< "$line")
-#     gene_start_position=$(awk '{print $2}' <<< "$line")   
-#     gene_end_position=$(awk '{print $3}' <<< "$line")   
-#     trinity_isoform_name=$(awk '{print $4}' <<< "$line")   
-#     trinity_gene_name=$(sed 's/_[^_]*$//'<<<$trinity_isoform_name)
-
-
-#     if [[ ! -z "$bedfile_chromosome" && ! -z "$gene_start_position" ]]
-#     then
-#     gene_start_position_start=$((gene_start_position - MARGIN))
-#     gene_start_position_end=$((gene_start_position + MARGIN))
-#     gene_end_position_start=$((gene_end_position - MARGIN))
-#     gene_end_position_end=$((gene_end_position + MARGIN))
-#     found_match=false
-# #   Iterate over the range
-#     for ((i = gene_start_position_start; i <= gene_start_position_end; i++)); do
-#         if grep -q  -E -m 1 "^$bedfile_chromosome\s+$i\s+" "$REFERENCE"
-#         then
-#             line=$(grep -E -m 1 "^$bedfile_chromosome\s+$i\s+" "$REFERENCE")
-#             new_string=$(sed 's/[[:space:]]\+/,/g' <<<"$line")
-#             echo "$bedfile_chromosome","$gene_start_position","$gene_end_position" ,"$trinity_isoform_name" ,"$trinity_gene_name","$new_string"
-#             found_match=true
-#             ((TOTAL_NUMBER_OF_ISOFORMS++))
-#             ((FOUND_ISOFORMS++))
-#             break
-#         else
-#             line=""
-#         fi
-#     done
-#     if ! $found_match
-#     then
-# for ((i = gene_end_position_start; i <= gene_end_position_end; i++)); do
-#     if grep -E -q -m 1 "${bedfile_chromosome}\s.*\s${i}" "$REFERENCE"; then
-#         line=$(grep -E -m 1 "${bedfile_chromosome}\s.*\s${i}" "$REFERENCE")
-#         new_string=$(sed 's/[[:space:]]\+/,/g' <<<"$line")
-#         echo "$bedfile_chromosome","$gene_start_position","$gene_end_position" ,"$trinity_isoform_name" ,"$trinity_gene_name","$new_string"
-#         found_match=true
-#         ((TOTAL_NUMBER_OF_ISOFORMS++))
-#         ((FOUND_ISOFORMS++))
-#         break
-#     else
-#         line=""
-#     fi
-# done
-# fi
-
-#     if ! $found_match 
-#     then
-#         ((TOTAL_NUMBER_OF_ISOFORMS++))
-#         echo "$bedfile_chromosome","$gene_start_position","$gene_end_position" ,"$trinity_isoform_name" ,"$trinity_gene_name","$line"
-#     fi
-#     fi  
-        
-# done < "$isoforms_locations"
-# # Print the first line in red
-# echo -e "\e[31m***********************************************************************************\e[0m"
-
-# # Print the second line in blue
-# echo -e "\e[34mTOTAL_NUMBER_OF_ISOFORMS: $TOTAL_NUMBER_OF_ISOFORMS\e[0m"
-
-# # Print the third line in blue
-# echo -e "\e[34mFOUND_ISOFORMS: $FOUND_ISOFORMS\e[0m"
