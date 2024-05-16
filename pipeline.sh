07b093813ec1a5b6cfe3817ed472d064ad75159b
Farhang <fjaryanius@gmail.com>
2024-05-16 10:12:46 -0500

    update the fucntions and test the program with trinity outputs

--- pipeline.sh
+++ pipeline.sh
@@ -2,21 +2,21 @@
 
 # Function to display usage information
 display_annotate_pipeline_usage() {
-# Red color
-echo -e "\e[31mOptions:\e[0m"
-echo -e "\e[31m  -h,--help             Display this help message\e[0m"
-echo -e "\e[31m  -o,--output           output_file_path                            default=current_directory\e[0m"
-echo -e "\e[31m  -f,--fraction         Similarity fraction                         default=0.9\e[0m"
-echo -e "\e[31m  -c,--FC-threshhold    FC threshhold                               default=7\e[0m"
-echo -e "\e[31m  -p,--prefix           prefix                                      default=\"annotated_\"\e[0m"
-echo -e "\e[31m  -i,--input-file       <isoforms_list.names | isoforms_table.csv>  Input isoforms_list file\e[0m"
-echo -e "\e[31m  -b,--bed              <provided_bedfile>                          Provided BED file\e[0m"
-echo -e "\e[31m  -r,--reference        <REF>                                       Reference FASTA file\e[0m"
-# Blue color
-echo -e "\e[34mSample command for run:\e[0m"
-echo -e "\e[34m./pipeline.sh -i list_isoforms.names -b Trinity.bed -r Reference.bed -o annotation_results -p Trinity_\e[0m"
-# Reset color
-echo -e "\e[0m"
+    # Display options in red color
+    echo -e "\e[31mOptions:\e[0m"
+    echo -e "\e[31m  -h,--help             Display this help message\e[0m"
+    echo -e "\e[31m  -o,--output           output_file_path                            default=current_directory\e[0m"
+    echo -e "\e[31m  -f,--fraction         Similarity fraction                         default=0.9\e[0m"
+    echo -e "\e[31m  -c,--FC-threshold     FC threshold                                default=7\e[0m"
+    echo -e "\e[31m  -p,--prefix           prefix                                      default=\"annotated_\"\e[0m"
+    echo -e "\e[31m  -i,--input-file       <isoforms_list.names | isoforms_table.csv>  Input isoforms_list file\e[0m"
+    echo -e "\e[31m  -b,--bed              <provided_bedfile>                          Provided BED file\e[0m"
+    echo -e "\e[31m  -r,--reference        <REF>                                       Reference FASTA file\e[0m"
+    # Display sample command in blue color
+    echo -e "\e[34mSample command for run:\e[0m"
+    echo -e "\e[34m./pipeline.sh -i list_isoforms.names -b Trinity.bed -r Reference.bed -o annotation_results -p Trinity_\e[0m"
+    # Reset color
+    echo -e "\e[0m"
 }
 
 # Initialize variables
@@ -25,13 +25,8 @@ PROVIDED_BEDFILE=""
 REFERENCE=""
 OUTPUT_FILE="$(pwd)"
 PREFIX="annotated_"
-TOTAL_NUMBER_OF_ISOFORMS=0
-FOUND_ISOFORMS=0
-FRACTION=.9
-NUMBER_OF_ISOFORMS=""
-ISOFORMS_NOT_FOUND=""
-ISOFORMS_TRINITY=""
-FOLD_CHANGE_THRESHHOLD="7"
+FRACTION=0.9
+FOLD_CHANGE_THRESHOLD=7
 
 # Parse command-line options
 while [[ $# -gt 0 ]]; do
@@ -44,8 +39,8 @@ while [[ $# -gt 0 ]]; do
             PROVIDED_BEDFILE=$2
             shift 2
             ;;
-         -c|--FC-threshhold )
-            FOLD_CHANGE_THRESHHOLD=$2
+        -c|--FC-threshold)
+            FOLD_CHANGE_THRESHOLD=$2
             shift 2
             ;;
         -r|--reference)
@@ -54,7 +49,7 @@ while [[ $# -gt 0 ]]; do
             ;;
         -o|--output)
             OUTPUT_FILE=$2
-            rm -r ${OUTPUT_FILE}
+            rm -rf ${OUTPUT_FILE}
             mkdir -p ${OUTPUT_FILE}
             shift 2
             ;;
@@ -77,66 +72,79 @@ while [[ $# -gt 0 ]]; do
             ;;
     esac
 done
+
+# Check if input file exists
 if [[ ! -f "${ISOFORM_INPUT_FILE}" ]]; then
-    echo "Error: Input file "${ISOFORM_INPUT_FILE}" does not exist."
+    echo "Error: Input file '${ISOFORM_INPUT_FILE}' does not exist."
     display_annotate_pipeline_usage
     exit 1
 fi
-# Check if the file is a CSV file based on its extension
 
+# Process the input file if it's a CSV file
 if [[ "${ISOFORM_INPUT_FILE##*.}" == "csv" ]]; then
-    python3 report_isoforms_names.py -f "${ISOFORM_INPUT_FILE}" -t "${FOLD_CHANGE_THRESHHOLD}"  -o ${OUTPUT_FILE}
+    python3 report_isoforms_names.py -f "${ISOFORM_INPUT_FILE}" -t "${FOLD_CHANGE_THRESHOLD}" -o ${OUTPUT_FILE}
     ISOFORM_INPUT_FILE="${OUTPUT_FILE}/csv_to_bed.bed"
 fi
+
 # Check if required arguments are provided
-if [[ -z $ISOFORM_INPUT_FILE || -z $PROVIDED_BEDFILE || -z $REFERENCE ]]
-then
+if [[ -z $ISOFORM_INPUT_FILE || -z $PROVIDED_BEDFILE || -z $REFERENCE ]]; then
     echo "Missing required arguments."
     display_annotate_pipeline_usage
     exit 1
 fi
+
+# Temporary file for isoform locations
 tmp_isoforms_locations=$(mktemp)
 isoforms_locations="${OUTPUT_FILE}/${PREFIX}isoforms_locations.txt"
-grep -v '^\s*$' "$ISOFORM_INPUT_FILE" >${tmp_isoforms_locations}
+
+# Remove empty lines from input file and write to temp file
+grep -v '^\s*$' "$ISOFORM_INPUT_FILE" > ${tmp_isoforms_locations}
 rm -f $isoforms_locations
-# Iterate through each line in the ISOFORM_INPUT_FILE file
+
+# Iterate through each isoform in the input file
 while IFS= read -r isoform; do
     # Check if the isoform exists in the provided BED file
     if grep -q -w "$isoform" "$PROVIDED_BEDFILE"; then
-        # Get the lines from the BED file matching the isoform
+        # Append matching lines from BED file to isoform locations
         grep -w "$isoform" "$PROVIDED_BEDFILE" >> $isoforms_locations
     else 
+        # If not found, add a placeholder entry
         echo -e "_\t _\t _\t ${isoform}" >> $isoforms_locations
     fi
 done < ${tmp_isoforms_locations}
 
+# Separate found and not found isoforms
 grep '^_*_*_' $isoforms_locations > "${OUTPUT_FILE}/${PREFIX}Not_found_isoforms.txt"
 ISOFORMS_NOT_FOUND=$(grep -c '^_*_*_' $isoforms_locations)
- 
+
+# Remove not found isoforms from the locations file
 grep -v '^_*_*_' ${isoforms_locations} > ${tmp_isoforms_locations}
 cat ${tmp_isoforms_locations} > ${isoforms_locations}
 
-echo -e "\e[31mTrinity Isoforms found  but not found in RefSeq \e[34m Fraction: ${FRACTION}\e[0m" > "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
-bedtools intersect -a "$isoforms_locations" -b mm39_genes.bed -f $FRACTION  -wa -wb  -v >> "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
+# Log and intersect with reference
+echo -e "\e[31mTrinity Isoforms found but not found in RefSeq \e[34m Fraction: ${FRACTION}\e[0m" > "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
+bedtools intersect -a "$isoforms_locations" -b mm39_genes.bed -f $FRACTION -wa -wb -v >> "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
 echo -e "\e[31mIsoforms intersection between Trinity and RefSeq \e[34m Fraction: ${FRACTION}\e[0m" >> "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
-bedtools intersect -a "$isoforms_locations" -b mm39_genes.bed -f $FRACTION  -wa -wb  >> "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
-awk '!seen[$1,$2,$3,$4]++' "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"  > "${OUTPUT_FILE}/${PREFIX}isoform_result.txt"
+bedtools intersect -a "$isoforms_locations" -b mm39_genes.bed -f $FRACTION -wa -wb >> "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt"
+awk '!seen[$1,$2,$3,$4]++' "${OUTPUT_FILE}/${PREFIX}log_all_matches.txt" > "${OUTPUT_FILE}/${PREFIX}isoform_result.txt"
+
+# Generate stats
 echo -e "\e[31m***************************************************************************************\e[0m" > "${OUTPUT_FILE}/${PREFIX}STATS.txt"
-NUMBER_OF_ISOFORMS="$(grep -v '^\s*$' ${ISOFORM_INPUT_FILE} | wc -l  | grep -o '[0-9]*' )"
+NUMBER_OF_ISOFORMS=$(grep -vc '^\s*$' ${ISOFORM_INPUT_FILE})
 awk '!seen[$4]++' "${OUTPUT_FILE}/${PREFIX}isoform_result.txt" > "${OUTPUT_FILE}/${PREFIX}uniq_isoform_result.txt"
 echo "Fraction Similarity: ${FRACTION}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
 echo "Total Number of Isoforms: ${NUMBER_OF_ISOFORMS}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
 echo "Isoforms Not Found: ${ISOFORMS_NOT_FOUND}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
 awk '{print $4}' "${OUTPUT_FILE}/${PREFIX}isoform_result.txt" | sort | uniq -c | awk '$1 > 1' > "${OUTPUT_FILE}/${PREFIX}duplicated_isoforms_names.txt"
-awk '{print$2}' "${OUTPUT_FILE}/${PREFIX}duplicated_isoforms_names.txt" | sort | uniq | grep -Ff - "${OUTPUT_FILE}/${PREFIX}isoform_result.txt" > "${OUTPUT_FILE}/${PREFIX}duplicated_isoforms.txt"
+awk '{print $2}' "${OUTPUT_FILE}/${PREFIX}duplicated_isoforms_names.txt" | sort | uniq | grep -Ff - "${OUTPUT_FILE}/${PREFIX}isoform_result.txt" > "${OUTPUT_FILE}/${PREFIX}duplicated_isoforms.txt"
 rm -f "${OUTPUT_FILE}/${PREFIX}duplicated_isoforms_names.txt"
 
-ISOFORMS_TRINITY=$(awk '/Trinity Isoforms found /{flag=1; next} /Isoforms intersection between Trinity and RefSeq/{ exit} {if(flag) {count++}} END {print count}' "${OUTPUT_FILE}/${PREFIX}isoform_result.txt")
-ISOFORMS_TRINITY_UNIQ=$(awk '/Trinity Isoforms found /{flag=1; next} /Isoforms intersection between Trinity and RefSeq/{ exit} {if(flag) {count++}} END {print count}' "${OUTPUT_FILE}/${PREFIX}uniq_isoform_result.txt")
+# Calculate additional stats
+ISOFORMS_TRINITY=$(awk '/Trinity Isoforms found/{flag=1; next} /Isoforms intersection between Trinity and RefSeq/{ exit} {if(flag) {count++}} END {print count}' "${OUTPUT_FILE}/${PREFIX}isoform_result.txt")
+ISOFORMS_TRINITY_UNIQ=$(awk '/Trinity Isoforms found/{flag=1; next} /Isoforms intersection between Trinity and RefSeq/{ exit} {if(flag) {count++}} END {print count}' "${OUTPUT_FILE}/${PREFIX}uniq_isoform_result.txt")
+total_isoform_found=$(wc -l < "${OUTPUT_FILE}/${PREFIX}isoform_result.txt")
+total_isoform_found_uniq=$(wc -l < "${OUTPUT_FILE}/${PREFIX}uniq_isoform_result.txt")
 echo "Number of Isoforms Exclusively Reported in Trinity: ${ISOFORMS_TRINITY}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
-total_isoform_found=$(wc -l "${OUTPUT_FILE}/${PREFIX}isoform_result.txt"| grep -o '[0-9]*')
-total_isoform_found_uniq=$(wc -l "${OUTPUT_FILE}/${PREFIX}uniq_isoform_result.txt"| grep -o '[0-9]*')
-echo "Number of Shared Isoforms: $((total_isoform_found -ISOFORMS_TRINITY -2 ))" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
+echo "Number of Shared Isoforms: $((total_isoform_found - ISOFORMS_TRINITY - 2))" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
 echo "Number of Unique Isoforms Exclusively Reported in Trinity: ${ISOFORMS_TRINITY_UNIQ}" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
-echo "Number of Shared Unique Isoforms: $((total_isoform_found_uniq - ISOFORMS_TRINITY_UNIQ -2 ))" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
-
+echo "Number of Shared Unique Isoforms: $((total_isoform_found_uniq - ISOFORMS_TRINITY_UNIQ - 2))" >> "${OUTPUT_FILE}/${PREFIX}STATS.txt"
--- report_isoforms_names.py
+++ report_isoforms_names.py
@@ -5,37 +5,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Define a command-line interface using Click
 @click.command()
 @click.option('-f', '--file_path', type=click.Path(exists=True), help='Path to the CSV file')
 @click.option('-t', '--threshold', default=7, help='Threshold value for significant Fold Change points')
 @click.option('-o', '--output', type=str, default='.', help='Path to save the chart and names file')
 def main(file_path, threshold, output):
+    # Check if the file path is provided
     if file_path is None:
         click.echo("Please provide the path to the CSV file using -f or --file_path option.")
         return
     
-    df = pd.read_csv(file_path)
-
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(file_path,sep='\s+')
+    #df = pd.read_csv(file_path, sep=",", engine='python')
+    # Extract columns from DataFrame
     logFC = df['logFC']
     P_Value = df['P.Value']
-
+    print(logFC)
     # Calculate -log10(P.Value)
     neg_log_P_Value = -np.log10(P_Value)
 
-    # Set threshold for significant points (commonly adjusted p-values < 0.05)
+    # Set threshold for significant points
     FC_significant_threshold = threshold
 
     # Define colors based on conditions
-    colors = np.where((logFC.abs() > FC_significant_threshold), 'black', np.where(logFC > 0, 'red', 'blue'))
+    colors = np.where(logFC > FC_significant_threshold, 'red', np.where(logFC < -FC_significant_threshold, 'blue', 'gray'))
 
     # Define legend labels
     legend_labels = {
-        'black': 'Significant',
+        'gray': 'Non-Significant',
         'red': 'Positive FC',
         'blue': 'Negative FC'
     }
 
-    # Print specifications of significant points
+    # Write significant points to a BED file
     filtered_lines = df[(abs(logFC) > FC_significant_threshold)]
     with open(f'{output}/csv_to_bed.bed', 'w') as f:
         for index, row in filtered_lines.iterrows():
@@ -46,12 +50,13 @@ def main(file_path, threshold, output):
     for color in np.unique(colors):
         plt.scatter(logFC[colors == color], neg_log_P_Value[colors == color], color=color, alpha=0.7, label=legend_labels[color])
 
-    plt.title('Volcano Plot for all sampled')
+    # Set plot title and axis labels
+    plt.title('Volcano Plot for all samples')
     plt.xlabel('logFC (Fold Change)')
     plt.ylabel('-log10(P.Value)')
     plt.legend()
     plt.grid(True)
-    plt.savefig(f'{output}/volcano_grouped.png')
+    plt.savefig(f'{output}/volcano_grouped.png')  # Save the plot as an image file
 
 if __name__ == "__main__":
-    main()
+    main()  # Execute the main function when the script is run
